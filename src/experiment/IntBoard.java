f3799f19c86f8c7f4541fc597a93691eced601fb
Connor McKay <connor@homeschooldebate.net>
2012-10-04 21:27:09 -0600

    Adding project and IntBoard class

---
+++ src/experiment/IntBoard.java
@@ -0,0 +1,31 @@
+package experiment;
+
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class IntBoard {
+
+	public IntBoard() {
+		// TODO implement
+	}
+	
+	public void calcAdjacencies() {
+		// TODO implement
+	}
+	
+	public void calcTargets(int startCell, int steps) {
+		// TODO implement
+	}
+	
+	public TreeSet getTargets() {
+		return new TreeSet();
+	}
+	
+	public LinkedList getAdjList(int cell) {
+		return new LinkedList();
+	}
+	
+	public static int calcIndex(int row, int col) {
+		return 0;
+	}
+}
