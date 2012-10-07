ffd3ef16313ffc66176be06609e7f8015c637b94
Connor McKay <connor@homeschooldebate.net>
2012-10-07 11:53:28 -0600

    Refactored calc adjacencies and formatted code

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -11,64 +11,72 @@ public class IntBoard {
 	private Map<Integer, LinkedList<Integer>> adjacencies;
 	private Set<Integer> targets;
 	private ArrayList<Boolean> seen;
-	private int COLS;
-	private int ROWS;
-	
+	private int columns;
+	private int rows;
+
 	public IntBoard() {
-		COLS=4;//for now, we hard code these.
-		ROWS=4;//Later, they will be read from a file.  
+		columns = 4;// for now, we hard code these.
+		rows = 4;// Later, they will be read from a file.
 		adjacencies = new TreeMap<Integer, LinkedList<Integer>>();
 		targets = new TreeSet<Integer>();
 		seen = new ArrayList<Boolean>();
-		for(Integer i=0;i<COLS*ROWS;i++)
+		for (Integer i = 0; i < columns * rows; i++)
 			seen.add(false);
 		calcAdjacencies();
 	}
-	
+
 	public void calcAdjacencies() {
-		for(int i=0; i<ROWS*COLS;i++){
-			LinkedList<Integer> lst = new LinkedList<Integer>();
-			int n = i-COLS;//neighbor (start by going up)
-			if(n>=0)lst.add(n);
-			n = i+COLS;//downstairs neighbor
-			if(n<=ROWS*COLS)lst.add(n);
-			n = i-1;//left neighbor
-			/*Logic below seems stupid, but with integer division
-			 * it actually determines the beginning of the row.
-			 */
-			if(n>=(i/COLS)*COLS)lst.add(n);
-			n = i+1;//right neighbor
-			/* Likewise, determine beg. of next row */
-			if(n<(i/COLS + 1)*COLS)lst.add(n);
-			adjacencies.put(i, lst);
+		for (int i = 0; i < rows * columns; i++) {
+			LinkedList<Integer> cells = new LinkedList<Integer>();
+			
+			int column = i % columns;
+			int row = i / columns;
+			
+			if (column > 0) { // left
+				cells.add(i - 1);
+			}
+			
+			if (column < columns - 1) { // right
+				cells.add(i + 1);
+			}
+			
+			if (row > 0) { // up
+				cells.add(i - columns);
+			}
+			
+			if (row < rows - 1) { // down
+				cells.add(i + columns);
+			}
+			
+			adjacencies.put(i, cells);
 		}
 	}
-	
+
 	public void calcTargets(int startCell, int steps) {
 		seen.set(startCell, true);
-		if(steps == 0){
+		if (steps == 0) {
 			targets.add(startCell);
 			seen.set(startCell, false);
-		}else
-		{
+		} else {
 			steps--;
-			for(Integer i : getAdjList(startCell)){
-				if(seen.get(i)==false) //will need extra conditions eventually
+			for (Integer i : getAdjList(startCell)) {
+				if (seen.get(i) == false) { // will need extra conditions eventually
 					calcTargets(i, steps);
+				}
 			}
 			seen.set(startCell, false);
 		}
 	}
-	
+
 	public TreeSet<Integer> getTargets() {
 		return (TreeSet<Integer>) targets;
 	}
-	
+
 	public LinkedList<Integer> getAdjList(int cell) {
 		return adjacencies.get(cell);
 	}
-	
+
 	public int calcIndex(int row, int col) {
-		return row*COLS+col;
+		return row * columns + col;
 	}
 }
