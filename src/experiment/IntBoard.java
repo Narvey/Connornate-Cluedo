dc036dc90b6ba7283d7b56223e6de622b7202df5
Connor McKay <connor@homeschooldebate.net>
2012-10-07 12:03:34 -0600

    More code cleanup

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,6 +1,5 @@
 package experiment;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
@@ -10,7 +9,7 @@ import java.util.TreeSet;
 public class IntBoard {
 	private Map<Integer, LinkedList<Integer>> adjacencies;
 	private Set<Integer> targets;
-	private ArrayList<Boolean> seen;
+	private boolean[] seen;
 	private int columns;
 	private int rows;
 
@@ -19,53 +18,52 @@ public class IntBoard {
 		rows = 4;// Later, they will be read from a file.
 		adjacencies = new TreeMap<Integer, LinkedList<Integer>>();
 		targets = new TreeSet<Integer>();
-		seen = new ArrayList<Boolean>();
-		for (Integer i = 0; i < columns * rows; i++)
-			seen.add(false);
+
+		seen = new boolean[rows*columns]; // initialized to false by default
+
 		calcAdjacencies();
 	}
 
 	public void calcAdjacencies() {
 		for (int i = 0; i < rows * columns; i++) {
 			LinkedList<Integer> cells = new LinkedList<Integer>();
-			
+
 			int column = i % columns;
 			int row = i / columns;
-			
+
 			if (column > 0) { // left
 				cells.add(i - 1);
 			}
-			
+
 			if (column < columns - 1) { // right
 				cells.add(i + 1);
 			}
-			
+
 			if (row > 0) { // up
 				cells.add(i - columns);
 			}
-			
+
 			if (row < rows - 1) { // down
 				cells.add(i + columns);
 			}
-			
+
 			adjacencies.put(i, cells);
 		}
 	}
 
 	public void calcTargets(int startCell, int steps) {
-		seen.set(startCell, true);
+		seen[startCell] = true;
 		if (steps == 0) {
 			targets.add(startCell);
-			seen.set(startCell, false);
 		} else {
 			steps--;
 			for (Integer i : getAdjList(startCell)) {
-				if (seen.get(i) == false) { // will need extra conditions eventually
+				if (seen[i] == false) { // will need extra conditions eventually
 					calcTargets(i, steps);
 				}
 			}
-			seen.set(startCell, false);
 		}
+		seen[startCell]  = false;
 	}
 
 	public TreeSet<Integer> getTargets() {
