d4f878ad82f8b5666ef3433895bf9c130368ea8f
N Harvey <nharvey@mymail.mines.edu>
2012-10-09 10:08:09 -0600

    ...

---
+++ src/clue/Board.java
@@ -0,0 +1,14 @@
+package clue;
+
+public class Board {
+
+	/**
+	 * @param args
+	 */
+	//string.split(',');
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
