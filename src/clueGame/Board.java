d9d4fda4415276a330ed8b7f65370b8f85f07665
N Harvey <nthnhrvy@gmail.com>
2012-10-16 12:18:35 -0600

    Added calcIndex, working on fixing tests

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -4,8 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Board {
 	ArrayList<BoardCell> cells;
@@ -15,13 +18,18 @@ public class Board {
 	String LegendFile;
 	String BoardFile;
 	Scanner scan;
+	private Map<Integer, LinkedList<Integer>> adjacencies;
+	private Set<BoardCell> targets;
+	private boolean[] seen;
 
 	public Board() {
 		cells = new ArrayList<BoardCell>();
 		rooms = new HashMap<Character, String>();
+		targets = new HashSet<BoardCell>();
 		LegendFile="legend.csv";
 		BoardFile="board.csv";
 		loadConfigFiles();
+		seen = new boolean[numRows*numColumns];
 	}
 
 	public void loadConfigFiles() {
@@ -30,9 +38,12 @@ public class Board {
 	}
 
 	public int calcIndex(int row, int column) {
-		return -400;// this SHOULD cause tests to fail.
+		return row*numColumns+column;
+		//TODO we are using rows+cols staring from 1, right?
 	}
-
+	public LinkedList<Integer> getAdjList(int index){
+		return adjacencies.get(index);
+	}
 	public RoomCell getRoomCellAt(int row, int column) {
 		return new RoomCell(row, column);
 	}
@@ -96,4 +107,40 @@ public class Board {
 		}
 
 	}
+	public void calcAdjacencies() {//TODO ripped from IntBoard.java
+		for (int i = 0; i < numRows * numColumns; i++) {
+			LinkedList<Integer> cells = new LinkedList<Integer>();
+
+			int column = i % numColumns;
+			int row = i / numColumns;
+
+			if (column > 0) { // left
+				cells.add(i - 1);
+			}
+
+			if (column < numColumns - 1) { // right
+				cells.add(i + 1);
+			}
+
+			if (row > 0) { // up
+				cells.add(i - numColumns);
+			}
+
+			if (row < numRows - 1) { // down
+				cells.add(i + numColumns);
+			}
+
+			adjacencies.put(i, cells);
+		}
+	}
+
+	public void calcTargets(int index, int moves) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Set<BoardCell> getTargets() {
+		return targets;
+	}
+
 }
