85f327498cbd29ffe97fd0563aa024c2bb0320d7
N Harvey <nthnhrvy@gmail.com>
2012-10-18 18:52:29 -0600

    adding pathsTests

---
+++ src/clueTests/PathsTests.java
@@ -0,0 +1,22 @@
+package clueTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+
+public class PathsTests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Board brd = new Board();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
